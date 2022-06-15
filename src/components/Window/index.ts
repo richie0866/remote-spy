54fc832b0b5e388454b5c71b80e07a96b22d102d
Richard <0866playback@gmail.com>
2022-06-14 23:22:32 -0700

    Remove WindowMask

--- src/components/Window/index.ts
+++ src/components/Window/index.ts
@@ -1,14 +1,12 @@
 import Window from "./Window";
 import WindowBackground from "./WindowBackground";
 import WindowDropShadow from "./WindowDropShadow";
-import WindowMask from "./WindowMask";
 import WindowResize from "./WindowResize";
 import WindowTitleBar from "./WindowTitleBar";
 
 export default {
 	Root: Window,
 	TitleBar: WindowTitleBar,
-	Mask: WindowMask,
 	Background: WindowBackground,
 	DropShadow: WindowDropShadow,
 	Resize: WindowResize,
